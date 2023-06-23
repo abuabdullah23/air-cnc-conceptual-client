18cea8f46625879c81b9d6940da536e498366d11
abuabdullah23 <abuobaidazayed@gmail.com>
2023-06-23 18:44:10 +0600

    Add footer, set conditional redering in category view

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Card from './Card';
 import Loader from '../Loader/Loader';
 import { useSearchParams } from 'react-router-dom';
+import Heading from '../Heading/Heading';
 
 const Rooms = () => {
     const [rooms, setRooms] = useState([]);
@@ -32,14 +33,27 @@ const Rooms = () => {
     }
 
     return (
-        <div className='my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+        <>
             {
-                rooms.map(room => <Card
-                    key={room._id}
-                    room={room}
-                ></Card>)
+                rooms && rooms.length > 0 ?
+                    <div className='my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+                        {
+                            rooms.map(room => <Card
+                                key={room._id}
+                                room={room}
+                            ></Card>)
+                        }
+                    </div>
+                    :
+                    <div className='my-16'>
+                        <Heading
+                            center={true}
+                            title={'No rooms available in this category'}
+                            subtitle={'Please select others category.'}
+                        ></Heading>
+                    </div>
             }
-        </div>
+        </>
     );
 };
 
